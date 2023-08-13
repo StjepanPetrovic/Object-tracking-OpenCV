b61389e608a751933e999f1e47d60a4485dcd841
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-08-13 23:16:54 +0200

    DELETE code and write code for accessing camera

--- main.py
+++ main.py
@@ -1,53 +1,20 @@
-import os
 import cv2
+import sys
 
-from zipfile import ZipFile
-from urllib.request import urlretrieve
+s = 0
+if len(sys.argv) > 1:
+    s = sys.argv[1]
 
-from matplotlib import pyplot as plt
+source = cv2.VideoCapture(s)
 
+win_name = 'Camera Preview'
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
-def download_and_unzip(url, save_path):
-    print(f"Downloading and extracting assets....", end="")
+while cv2.waitKey(1) != 27: # Escape
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+    cv2.imshow(win_name, frame)
 
-    urlretrieve(url, save_path)
-
-    try:
-        with ZipFile(save_path) as z:
-            z.extractall(os.path.split(save_path)[0])
-
-        print("Done")
-
-    except Exception as e:
-        print("\nInvalid file.", e)
-
-
-URL = r"https://www.dropbox.com/s/qhhlqcica1nvtaw/opencv_bootcamp_assets_NB1.zip?dl=1"
-
-asset_zip_path = os.path.join(os.getcwd(), "opencv_bootcamp_assets_NB1.zip")
-
-if not os.path.exists(asset_zip_path):
-    download_and_unzip(URL, asset_zip_path)
-else:
-    print("\nZip already exists.")
-
-img = cv2.imread("checkerboard_18x18.png", cv2.IMREAD_GRAYSCALE)
-
-# if this is not converted, BGR color space is there and therefor image is yellow and purple
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-plt.imshow(img, cmap="gray")
-plt.show()
-
-img_copy = img.copy()
-img_copy[2, 2] = 200
-img_copy[2, 3] = 200
-img_copy[3, 2] = 200
-img_copy[3, 3] = 200
-
-# Same as above
-# img_copy[2:3,2:3] = 200
-
-plt.imshow(img_copy, cmap="gray")
-plt.show()
-print(img_copy)
+source.release()
+cv2.destroyWindow(win_name)
