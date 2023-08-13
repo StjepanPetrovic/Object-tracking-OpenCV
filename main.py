7e364bb90fc85ff9fb777052e073cbab68bd512c
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-08-13 20:34:12 +0200

    Display images directly in image viewer

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from zipfile import ZipFile
 from urllib.request import urlretrieve
 
+from PIL import Image
+
 
 def download_and_unzip(url, save_path):
     print(f"Downloading and extracting assets....", end="")
@@ -25,3 +27,12 @@ asset_zip_path = os.path.join(os.getcwd(), "opencv_bootcamp_assets_NB1.zip")
 
 if not os.path.exists(asset_zip_path):
     download_and_unzip(URL, asset_zip_path)
+else:
+    print("\nZip already exists.")
+
+image_checkerboard18x18 = Image.open("checkerboard_18x18.png")
+image_checkerboard18x18.show()
+
+image_checkerboard84x84 = Image.open("checkerboard_84x84.jpg")
+image_checkerboard84x84.show()
+
