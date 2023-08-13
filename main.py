cca1e456f5d145cf2092ed07e85300e2eac43a13
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-08-13 21:18:49 +0200

    Display coca-cola image with matplotlib
    The color displayed is different from the actual image. This is because matplotlib expects the image in RGB format whereas OpenCV stores images in BGR format. Thus, for correct display, we need to reverse the channels of the image. We will discuss about the channels in the sections below.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 from zipfile import ZipFile
 from urllib.request import urlretrieve
 
+from matplotlib import pyplot as plt
 
 
 def download_and_unzip(url, save_path):
@@ -36,3 +37,15 @@ cb_img = cv2.imread("checkerboard_18x18.png", 0)
 # Print the image data (pixel values), element of a 2D numpy array.
 # Each pixel value is 8-bits [0,255]
 print(cb_img)
+
+# Read in image
+coke_img = cv2.imread("coca-cola-logo.png", 1)
+
+# print the size (height, width, number of color channels - red, green, blue) of image
+print("\nImage size (H, W, C) is:", coke_img.shape)
+
+# print data-type of image
+print("Data type of image is:", coke_img.dtype)
+
+plt.imshow(coke_img)
+plt.show()
