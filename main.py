c4e044e940076869c02b0ee822ea87da1edf4006
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-08-14 01:53:50 +0200

    Open generated video

--- main.py
+++ main.py
@@ -144,3 +144,36 @@ while True:
 
 video.release()
 video_out.release()
+
+
+# open generated video
+
+cap = cv2.VideoCapture(video_output_file_name)
+
+# Check if camera opened successfully
+if not cap.isOpened():
+    print("Error opening video file")
+
+# Read until video is completed
+while cap.isOpened():
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret:
+        # Display the resulting frame
+        cv2.imshow('Frame', frame)
+
+        # Press Q on keyboard to exit
+        if cv2.waitKey(50) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else:
+        break
+
+# When everything done, release
+# the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
